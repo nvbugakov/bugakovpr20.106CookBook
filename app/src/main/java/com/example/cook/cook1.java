a3cf9a57f83fcf2103d7c8c37769bb5514b25bd9
Bugakov Nikita <nvbugakov@mail.ru>
2021-05-12 13:15:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/cook/cook1.java
@@ -0,0 +1,14 @@
+package com.example.cook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cook1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cook1);
+    }
+}
